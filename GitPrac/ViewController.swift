44d478f5c249c43b8f8f2df0dc8db3a9889b8d8f
OneCookieLeft <benjamin.setab@gmail.com>
2022-10-06 17:53:20 -0500

    Initial Commit

---
+++ GitPrac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPrac
+//
+//  Created by Benjamin Bates on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
